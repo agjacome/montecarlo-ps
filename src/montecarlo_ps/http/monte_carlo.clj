a6d5329494a24c5e9392bfd4075da80225700e8c
Susan Henriquez <susan.henriquez@thoughtworks..com>
2019-11-11 17:50:43 +0100

    Add parameters to submitted page

--- src/montecarlo_ps/http/monte_carlo.clj
+++ src/montecarlo_ps/http/monte_carlo.clj
@@ -1,18 +1,22 @@
 (ns montecarlo-ps.http.monte-carlo
   (:require [compojure.core :refer [GET, POST, defroutes]]
-            [selmer.parser :refer [render-file]]))
+            [selmer.parser :refer [render-file]]
+            [clojure.walk :refer [keywordize-keys]]
+            [ring.util.codec :refer [form-decode]]))
 
+(defn- parse-form-data [request]
+  (keywordize-keys (form-decode (:body request))))
 
 (defn form-page [request]
-  {:status 200
+  {:status  200
    :headers {"Content-Type" "text/html"}
-   :body (render-file "montecarlo_ps/http/form.html" {})})
+   :body    (render-file "montecarlo_ps/http/form.html" {})})
 
 (defn form-page-submission [request]
-  {:status 200
+  {:status  200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>It has worked</h1>"})
+   :body    (render-file "montecarlo_ps/http/submitted_page.html" (parse-form-data request))})
 
 (defroutes routes
-  (GET  "/" [] form-page            )
-  (POST "/submit"    [] form-page-submission))
+  (GET  "/"       [] form-page)
+  (POST "/submit" [] form-page-submission))
--- test/montecarlo_ps/http/monte_carlo_test.clj
+++ test/montecarlo_ps/http/monte_carlo_test.clj
@@ -4,22 +4,22 @@
             [selmer.parser :refer [render-file]]))
 
 (defn- request
-  ([] (request :get "/"))
-  ([method] (request method "/"))
-  ([method path] {:request-method method :uri path}))
+  ([]                 (request :get "/" ""))
+  ([method]           (request method "/" ""))
+  ([method path]      (request method path ""))
+  ([method path body] {:request-method method :uri path :body body}))
 
 (facts "about `monte-carlo/form-page`"
-       (fact "it always returns an OK response"
-             (let [response (monte-carlo/form-page request)]
-               (:status response) => 200
-               (:headers response) => {"Content-Type" "text/html"}
-               (:body response) => (render-file "montecarlo_ps/http/form.html" {}))))
+   (fact "it always returns an OK response"
+       (let [response (monte-carlo/form-page request)]
+          (:status response)  => 200
+          (:headers response) => {"Content-Type" "text/html"}
+          (:body response)    => (render-file "montecarlo_ps/http/form.html" {}))))
 
 
 (facts "about `monte-carlo/submit`"
-       (fact "it returns the correct html"
-             (let [response (monte-carlo/form-page-submission(request :post "/submit"))]
-               (:status response) => 200
-               (:headers response) => {"Content-Type" "text/html"}
-               (:body response) => "<h1>It has worked</h1>"
-               )))
+   (fact "it returns the correct html"
+       (let [response (monte-carlo/form-page-submission (request :post "/submit" "startDate=wefwef"))]
+           (:status response)  => 200
+           (:headers response) => {"Content-Type" "text/html"}
+           (:body response)    => "<h1>Start date: wefwef</h1>")))
