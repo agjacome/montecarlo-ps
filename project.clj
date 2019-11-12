5066897049e365c856b8f4f1db5423fa3c62a1e1
Alberto G. Jácome <agjacome@gmail.com>
2019-11-12 18:53:14 +0100

    Enable kibit static analyzer before running tests

--- project.clj
+++ project.clj
@@ -14,6 +14,7 @@
   :plugins [[jonase/eastwood            "0.3.5" ]
             [lein-ancient               "0.6.15"]
             [lein-cljfmt                "0.6.5" ]
+            [lein-kibit                 "0.1.7" ]
             [lein-midje                 "3.2.1" ]
             [me.arrdem/lein-git-version "2.0.8" ]]
 
@@ -21,7 +22,7 @@
              :only-modified true}
 
   :aliases {"format" ["cljfmt" "fix"]
-            "lint"   ["eastwood"]
+            "lint"   ["do" ["kibit"] ["eastwood"]]
             "test"   ["do" ["format"] ["lint"] ["midje"]]}
 
   :main montecarlo-ps.main)
