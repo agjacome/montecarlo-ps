9f1e3ebe393f409208cd4e9efdb1c78be7843925
Alberto G. Jácome <agjacome@gmail.com>
2019-11-26 02:40:38 +0100

    Move Ring Middlewares to top-level application and routes

--- src/montecarlo_ps/endpoints/form.clj
+++ src/montecarlo_ps/endpoints/form.clj
@@ -1,8 +1,6 @@
 (ns montecarlo-ps.endpoints.form
   (:require [compojure.core :refer [GET, POST, defroutes]]
-            [selmer.parser :refer [render-file]]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.multipart-params :refer [wrap-multipart-params]]))
+            [selmer.parser :refer [render-file]]))
 
 (defn get-handler [request]
   {:status  200
@@ -12,11 +10,11 @@
 (defn post-handler [request]
   (let [request-params (:params request)
         start-date     (request-params "startDate")
-        csv-content    (slurp (:tempfile (request-params "csv")))]
+        csv-content    (slurp (:tempfile (get request-params "csv")))]
     {:status  200
      :headers {"Content-Type" "text/html"}
      :body    (render-file "montecarlo_ps/pages/results.html" {:content csv-content :startDate start-date})}))
 
 (defroutes routes
   (GET "/" [] get-handler)
-  (wrap-multipart-params (wrap-params (POST "/" [] post-handler))))
+  (POST "/" [] post-handler))
--- src/montecarlo_ps/main.clj
+++ src/montecarlo_ps/main.clj
@@ -1,9 +1,13 @@
 (ns montecarlo-ps.main
   (:require [environ.core :refer [env]]
-            [montecarlo-ps.endpoints.routes :refer [routes]]
-            [org.httpkit.server :as server]))
+            [org.httpkit.server :as server]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [montecarlo-ps.endpoints.routes :refer [routes]]))
+
+(def application (-> routes wrap-params wrap-multipart-params))
 
 (defn -main [& args]
   (let [port (Integer/parseInt (or (env :port) "8080"))]
-    (server/run-server #'routes {:port port})
+    (server/run-server #'application {:port port})
     (println (str "Webserver started at http://localhost:" port "/"))))
